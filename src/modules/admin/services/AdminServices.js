bb6720ff2a30f5a9fd86dcabf5738aa4f5f25da4
ChristianAlonso16 <105463895+ChristianAlonso16@users.noreply.github.com>
2024-10-23 12:29:33 -0600

    add: consume login

---
+++ src/modules/admin/services/AdminServices.js
@@ -0,0 +1,20 @@
+import axiosPrivate from '../../../core/api/HttpPrivate'
+
+const loginAdmin = async (email, password) => {
+    try {
+      const data = {
+        username: email,
+        password: password,
+      };
+      const response = await axiosPrivate.post("/auth/login", data);
+      
+      return response.data;
+    } catch (error) {
+      return error
+    }
+  };
+  
+  export default {
+    loginAdmin,
+  };
+  
